445d81de62256f5c7e6e94447b7522d82ffa4b06
Christopher <Christopher_do@outlook.com>
2026-09-17 11:44:32 +1000

    Initial Commit

---
+++ DECO2200/DECO2200App.swift
@@ -0,0 +1,17 @@
+//
+//  DECO2200App.swift
+//  DECO2200
+//
+//  Created by Christopher Do on 17/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct DECO2200App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DECO2200UITests/DECO2200UITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  DECO2200UITestsLaunchTests.swift
+//  DECO2200UITests
+//
+//  Created by Christopher Do on 17/9/2026.
+//
+
+import XCTest
+
+final class DECO2200UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
